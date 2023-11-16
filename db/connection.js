4f27a1f578b6682eb8ebf398e52e30d19c0a01fe
Hailey Strauss <hstrauss2001@gmail.com>
2023-11-15 21:08:22 -0500

    update

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,10 @@
-const mysql = require("mysql2");
-
 const connection = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "localhost",
   // Your username
   user: "root",
   // Your password
-  password: "",
+  password: "yourPasswordHere",
   database: "employee_db",
-});
-
-connection.connect(function (err) {
-  if (err) throw err;
 });
 
 module.exports = connection;
