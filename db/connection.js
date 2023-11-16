afc22f76e4cf8a126350f037b69928e447b28bda
Hailey Strauss <hstrauss2001@gmail.com>
2023-11-15 21:14:27 -0500

    update

--- db/connection.js
+++ db/connection.js
@@ -8,4 +8,8 @@ const connection = mysql.createConnection({
   database: "employee_db",
 });
 
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+});
 module.exports = connection;
